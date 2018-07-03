89c2e680ca6fe6545af3c4dd7c7b2b78d82b79de
Thomas Santillan <tsantillan93@gmail.com>
2018-07-03 14:44:56 -0400

    fixes

--- JenkinsProject/src/main/java/com/revature/beans/Bean.java
+++
@@ -1,9 +0,0 @@
-package com.revature.beans;
-
-public class Bean {
-// this is a test class
-//David Was here
-//Robert has joined the party - now with his own branch
-	//Tye
-	//thomas testing again
-}
