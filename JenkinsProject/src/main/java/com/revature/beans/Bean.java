b07b5b5a87baf9194175182a015314299e88c080
Robert Crawford <RobertDev@Roberts-MacBook-Air.local>
2018-07-02 20:41:59 -0400

    Testing to see if my branch works

--- JenkinsProject/src/main/java/com/revature/beans/Bean.java
+++ JenkinsProject/src/main/java/com/revature/beans/Bean.java
@@ -3,6 +3,6 @@ package com.revature.beans;
 public class Bean {
 // this is a test class
 //David Was here
-//Robert has joined the party
+//Robert has joined the party - now with his own branch
 	//Tye
 }
