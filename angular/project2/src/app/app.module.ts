8c809671ff6d788f6903a513507efe5c91978d28
david <dvmill1994@gmail.com>
2018-07-05 21:30:28 -0400

    fixing

--- angular/project2/src/app/app.module.ts
+++ angular/project2/src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent,
+   LoginComponent
+ ],
+ imports: [
+   BrowserModule
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
