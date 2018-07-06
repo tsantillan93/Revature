0f7c54d677de1b1e81b069604192cf482cb718b9
Tyerra Smith <tyerra.monae@gmail.com>
2018-07-05 21:40:47 -0400

    trying to resolve conflicts - Tye

--- angular/project2/src/app/app.module.ts
+++ angular/project2/src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegistationComponent } from './registation/registation.component';
-
-import { UserserviceService } from './userservice.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegistationComponent
+    LoginComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    UserserviceService
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
