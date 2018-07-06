a0ddaee1c77ec32f494976ec7cf7581590e16fbd
Thomas Santillan <tsantillan93@gmail.com>
2018-07-06 13:49:42 -0400

    merge fixes

--- angular/project2/src/app/app.module.ts
+++ angular/project2/src/app/app.module.ts
@@ -1,14 +1,31 @@
-import { BrowserModule } from ‘@angular/platform-browser’;
-import { NgModule } from ‘@angular/core’;import { AppComponent } from ‘./app.component’;
-import { LoginComponent } from ‘./login/login.component’;@NgModule({
- declarations: [
-   AppComponent,
-   LoginComponent
- ],
- imports: [
-   BrowserModule
- ],
- providers: [],
- bootstrap: [AppComponent]
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+// custom modules
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+//import { RegistationComponent } from './registation/registation.component';
+import { NavbarComponent } from './navbar/navbar.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    //RegistationComponent,
+    HomeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
