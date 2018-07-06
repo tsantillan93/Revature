3e7e7d6bdbc08bfac7a72f2f1bf67d9e15afe6c6
david <dvmill1994@gmail.com>
2018-07-05 21:47:48 -0400

    did what told

--- angular/project2/src/app/app.module.ts
+++ angular/project2/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
- declarations: [
-   AppComponent,
-   LoginComponent
- ],
- imports: [
-   BrowserModule
- ],
- providers: [],
- bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
