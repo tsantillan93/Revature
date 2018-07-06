91dd822c3431a2be0d09a0e7b2f9ca5332835bbd
Robert Crawford <RobertDev@Roberts-MacBook-Air.local>
2018-07-06 13:40:54 -0400

    solving merge conflict

--- angular/project2/src/app/app.module.ts
+++ angular/project2/src/app/app.module.ts
@@ -1,24 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { UserEditorComponent } from './user-editor/user-editor.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    UserEditorComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+import { BrowserModule } from ‘@angular/platform-browser’;
+import { NgModule } from ‘@angular/core’;import { AppComponent } from ‘./app.component’;
+import { LoginComponent } from ‘./login/login.component’;@NgModule({
+ declarations: [
+   AppComponent,
+   LoginComponent
+ ],
+ imports: [
+   BrowserModule
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
