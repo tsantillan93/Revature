bbaccd25014bfd619a29c98ed6646a2dd6a85579
Thomas Santillan <tsantillan93@gmail.com>
2018-07-05 20:48:58 -0400

    first attempt at angular git

--- JenkinsProject/src/main/java/com/revature/controller/HomeController.java
+++ JenkinsProject/src/main/java/com/revature/controller/HomeController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,6 +15,7 @@ import com.revature.beans.User;
 import com.revature.services.HomeService;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomeController
 {
         @Autowired
--- angular/project2/src/app/app.module.ts
+++ angular/project2/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
