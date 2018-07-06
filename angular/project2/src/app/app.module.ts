c56030d9d276dbe7b10bbd42f0d8f4644b6092d5
Tyerra Smith <tyerra.monae@gmail.com>
2018-07-05 20:54:21 -0400

    Registration Component - Tye

--- angular/project2/src/app/app.module.ts
+++ angular/project2/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegistationComponent } from './registation/registation.component';
+
+import { UserserviceService } from './userservice.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    UserserviceService
   ],
   providers: [],
   bootstrap: [AppComponent]
